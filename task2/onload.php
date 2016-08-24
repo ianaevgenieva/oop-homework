da2b0aa016b3a0a6fa9323331f89ad2b8260ae70
Iana Qkimova <ianaevgenieva@mail.bg>
2016-08-24 22:24:09 +0300

    gd

---
+++ task2/onload.php
@@ -0,0 +1,12 @@
+<?php
+
+
+/* function __autoload($class) {
+ $class = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php' ;
+ require_once __DIR__ . DIRECTORY_SEPARATOR . $class;
+ } */
+
+spl_autoload_register(function ($class) {
+	$class = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php' ;
+	require_once __DIR__ . DIRECTORY_SEPARATOR . $class;
+});
